8678776d3fd54249f5b2fded3ae81e221c30151e
kmcpeek <kevin.j.mcpeek@live.com>
2018-04-22 19:30:36 -0500

    Some minor style changes

--- src/main/java/anthropaedic/pvpbot/Pvpbot.java
+++ src/main/java/anthropaedic/pvpbot/Pvpbot.java
@@ -2,6 +2,8 @@ package anthropaedic.pvpbot;
 
 import javax.security.auth.login.LoginException;
 
+import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+
 import anthropaedic.pvpbot.listener.MessageManager;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDA;
@@ -12,38 +14,42 @@ public class Pvpbot {
 	private String token;
 	private JDA api;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		Pvpbot myBot = new Pvpbot();
 	}
 	
-	public Pvpbot() {
+	public Pvpbot() 
+	{
 		this("NDM3MzQ1ODc5NzMzNjk4NTcy.Db1HbA.oqBigO58PVrYxIlIBEcv_8t3YRs");
 	}
 	
-	public Pvpbot(String token) {
+	public Pvpbot(String token) 
+	{
 		this.token = token;
 		login();
 		registerListeners();
-		}
+	}
 	
 	private void login() {
-		try {
+		try 
+		{
 			api = new JDABuilder(AccountType.BOT)
 					.setToken(token)
 					.setEventManager(new AnnotatedEventManager())
-					.buildBlocking();
-		} catch (LoginException | IllegalArgumentException e) {
+					.buildAsync();
+		} 
+		catch (LoginException | IllegalArgumentException e) 
+		{
 			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			e.printStackTrace();	
 		}
-		
 	}
 	
-	private void registerListeners() {
-		api.addEventListener(new MessageManager());
+	private void registerListeners() 
+	{
+		
+		api.addEventListener(new EventWaiter());
 	}
 	
 
